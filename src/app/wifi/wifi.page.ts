3ef89dbc7ccc3b624f4b74411d7c71ff4b23884c
VinceMarquez <61826770+VinceMarquez@users.noreply.github.com>
2020-03-12 11:55:40 +0800

    added wifi list syntax

--- src/app/wifi/wifi.page.ts
+++ src/app/wifi/wifi.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Hotspot, HotspotNetwork } from '@ionic-native/hotspot/ngx';
+import { Platform } from '@ionic/angular'
 
 @Component({
   selector: 'app-wifi',
@@ -7,8 +9,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./wifi.page.scss'],
 })
 export class WifiPage {
+  networks:HotspotNetwork[]=[];
 
-  constructor(private router: Router) { }
+  constructor(private router:Router, public platform:Platform, public hotspot:Hotspot) 
+  { 
+    this.platform.ready().then(()=>{
+      this.hotspot.scanWifi().then((networks:HotspotNetwork[])=>{
+        this.networks = networks;
+      })
+    })
+  }
 
   gotomain() {
     this.router.navigate(['main']);
