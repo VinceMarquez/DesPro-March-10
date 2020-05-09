fc0d0c18d4afde18a0dafd75ef3e782dff2e9b56
VinceMarquez <61826770+VinceMarquez@users.noreply.github.com>
2020-05-09 22:05:33 +0800

    added new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'turnoff',
     loadChildren: () => import('./turnoff/turnoff.module').then( m => m.TurnoffPageModule)
   },
+  {
+    path: 'password',
+    loadChildren: () => import('./password/password.module').then( m => m.PasswordPageModule)
+  },
 
 ];
 
